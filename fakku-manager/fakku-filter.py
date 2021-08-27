23445e4205e876138ac251e5eda8a4a1e8b6ecfe
Freddy930727 <930727fre@gmail.com>
2021-08-27 19:38:42 +0800

    stable release v3

--- fakku-manager/fakku-filter.py
+++ fakku-manager/fakku-filter.py
@@ -6,6 +6,9 @@ print("cwd:"+cwd)
 print()
 input("press any key to continue or ctrl+c to suspend")
 
+
+
+
 if(os.path.isfile(cwd+"/config.ini"))==False:
     f=open("config.ini","a")
     f.write("[img]")
@@ -13,14 +16,26 @@ if(os.path.isfile(cwd+"/config.ini"))==False:
 
 config = configparser.ConfigParser()
 config.read('config.ini')
+ls=config.items("img")
+print(len(ls))
 
 ans=[]
 foldernames=[]
+counter=0
+temp=0
+
+
+def save():
+    for i in range(len(foldernames)):
+        config["img"][str(temp)]=ans[i]
+        temp=temp+1
+
 
 for root,dirs,files in os.walk(cwd):
-    foldername=root.split(os.path.sep)[-1]
-    if dirs==[] and not foldername=="output":
-        if len(files)>10:
+    dirs.sort()
+    if counter>len(ls):
+        foldername=root.split(os.path.sep)[-1]
+        if dirs==[]:
             files.sort()
             imagedir=os.path.join(root,files[0])
             print(foldername)
@@ -30,15 +45,10 @@ for root,dirs,files in os.walk(cwd):
             while temp != "1" and temp != "2":
                 print("error! please input 1 or 2")
                 temp=input("ans:")
-            ans.append(temp)
-print(ans)
-print(foldernames)
+            config["img"][str(counter-1)]=str(temp)
+            with open('config.ini', 'w') as f:
+                config.write(f)
+    counter=counter+1
 
-temp=0
 
-for i in range(len(foldernames)):
-    config["img"][str(temp)]=ans[i]
-    temp=temp+1
-with open('config.ini', 'w') as f:
-    config.write(f)
 
--- fakku-manager/fakku-manager.py
+++ fakku-manager/fakku-manager.py
@@ -4,87 +4,57 @@ from PyPDF2 import PdfFileMerger
 if(os.path.isfile(os.getcwd()+"/config.ini"))==False:
     print("config.ini doesn't exist.")
 else:
-    
-    print("cwd:"+os.getcwd())
     title=input("please enter title for the output files:")
-
-    if title=="":
+    if (title==""):
         for i in range(len(os.getcwd().split("/"))-1,0,-1):
             if "fakku" in os.getcwd().split("/")[i]:
                 title=os.getcwd().split("/")[i]
                 break
     print("title:"+title)
     input("press any key to continue or ctrl+c to suspend")
-    
     config = configparser.ConfigParser()
     config.read('config.ini')
-    
     start=time.time()
-
     counter=0
     num=0
     passed=False
-
-    if not os.path.exists(os.getcwd()+"/output"):
-        os.mkdir(os.getcwd()+"/output")
     for root,dirs,files in os.walk(os.getcwd()):
+        dirs.sort()
         ls=[]
         foldername=root.split(os.path.sep)[-1]
-        if foldername=="output":
-            passed=True
-        if dirs==[] and not foldername=="output":
-            if passed==True:
-                temp=num-1
-            else:
-            	temp=num
-            	
-            if config["img"][str(temp)]=="1":
+        if dirs==[]:
+            if config["img"][str(num)]=="1":
                 counter=counter+1
                 print("process:"+str(counter))
                 files.sort()
                 for i in files:
-                    #print(os.path.join(root,i))
                     ls.append(os.path.join(root,i))
                 print(foldername)
-                #time.sleep(5)
-                with open("./output/"+foldername+".pdf", "wb") as f:
+                with open(foldername+".pdf", "wb") as f:
                     f.write(img2pdf.convert([i for i in ls if i.endswith(".png")],compress=True))
-                #time.sleep(5)
             num=num+1
     merger = PdfFileMerger()
-
-    outputpath=os.getcwd()+"/output"
-
+    outputpath=os.getcwd()
     ls=[]
-
     for root,dirs,files in os.walk(outputpath):
         for i in files:
-            ls.append("./output/"+i)
-
-
+            ls.append(i)
+    merge=[]
     for pdf in ls:
-        merger.append(pdf)
-
+        if(".pdf" in pdf and pdf!=title+"_all.pdf"):
+            merger.append(pdf)
+            merge.append(pdf)
     merger.write(title+"_all.pdf")
     merger.close()
-
-
-
-    for j in range(int(len(ls)/10)+1):
+    for j in range(int(len(merge)/10)+1):
         merger = PdfFileMerger()
-        
-        
         for k in range(10):
             index=k+j*10
-            if index==len(ls):
+            if index==len(merge):
                 break
-            merger.append(ls[index])
+            merger.append(merge[index])
         merger.write(title+"_pt."+str(j+1)+".pdf")
         merger.close()
-            
-
-
     end=time.time()
-    print(end-start)
     print("it takes "+str(int(end-start))+" seconds to finish the process.")
 
